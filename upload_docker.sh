3d28e94a8f3a6545466b5f96bf684e69458a6ed5
queens kisivuli <queenskisivuli@gmail.com>
2023-02-14 18:27:10 +0300

    upload docker img to docker hub

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=queensk/ml-api
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker --tag ml-api $dockerpath:latest
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:latest
