a03d6fc29dc5eb7c92b6a9b818bf9a5064d3d6d5
queens kisivuli <queenskisivuli@gmail.com>
2023-02-15 02:49:57 +0300

    upload image to kubernetes pods

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,18 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=queensk/ml-api
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run ml-api\
+	--image=$dockerpath\
+	--port=80 --labels app=ml-api
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward ml-api 8000:80
